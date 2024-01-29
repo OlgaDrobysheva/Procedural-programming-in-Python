7ed6feb831d68982fef340d7c99da222aa435adf
Olga <122975253+OlgaDrobysheva@users.noreply.github.com>
2024-01-29 15:56:43 +0300

    Update task_2.py.py

--- lab_4/task_2.py.py
+++ lab_4/task_2.py.py
@@ -1,20 +1,14 @@
+# TODO решите задачу
 import json
 
-INPUT_FILE = "input.json"
+
+def task() -> float:
+    with open('input.json', 'r') as file:
+        data = json.load(file)
+
+    sum_of_products = sum(item["score"] * item["weight"] for item in data)
+    result = round(sum_of_products, 3)
+    return result
 
 
-# TODO решите задачу
-def task(json_file) -> float:
-    with open(json_file, 'r') as file:
-        json_data = json.load(file)
-    sum_of_products = 0
-    for record in json_data:
-        score = float(record.get('score', 0))
-        weight = float(record.get('weight', 0))
-        product = score * weight
-        sum_of_products += product
-    rounded_sum = round(sum_of_products, 3)
-    return rounded_sum
-
-
-print(task(INPUT_FILE))
+print(task())
